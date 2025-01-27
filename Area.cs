d71ce569929163342f5b5623113b3fd961c47e97
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-01-27 16:46:50 -0600

    Add files via upload

---
+++ Area.cs
@@ -0,0 +1,42 @@
+﻿//Written By Kadin Meyer
+// 1/27/2025
+
+using System;
+using System.Collections.Generic;
+
+namespace AreaOfTriangle
+{
+    public class Area
+    {
+        // Declaration of Class Variables
+        private int baseTri = -10000;
+        private int heightTri = -10000;
+
+
+        //Gets and Sets
+
+        public int BaseTri 
+        { 
+            get { return this.baseTri; } 
+            set { this.baseTri = value; }
+        }
+
+        public int HeightTri
+        {
+            get { return this.heightTri; }
+            set { this.heightTri = value; }
+        }
+
+        //Methods
+
+        public double calcArea() 
+        {
+           double area = 0;
+            area = (BaseTri*HeightTri)/2;
+            return area;
+        
+        }   
+
+
+    }
+}
